45be3cc32b0c56a73b63facc8ea33ded454491a5
Annisa Fairuz <annisa_fairuz_27rpl@student.smktelkom-mlg.sch.id>
2023-07-13 13:10:37 +0700

    Migrations

---
+++ database/migrations/2023_07_13_053740_create_rents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lifebuoy_id')->constrained();
+            $table->foreignId('visitor_id')->constrained();
+            $table->time('borrow');
+            $table->time('deadline');
+            $table->integer('is_delete');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rents');
+    }
+};
