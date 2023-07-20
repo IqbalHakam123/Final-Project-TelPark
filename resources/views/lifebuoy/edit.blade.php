62fc081f7d79991f739bdc7e70ccb446c0d2c985
Rafly Akbar <raflyakbarrafsanjani@gmail.com>
2023-07-20 18:38:04 +0700

    Lifebuoy Page

--- resources/views/lifebuoy/edit.blade.php
+++ resources/views/lifebuoy/edit.blade.php
@@ -9,6 +9,11 @@
             <div class="col-lg-9 col-xl-10">
                 <h4 class="mb-3" style="color:#6AB0BE"><b>{{$pageTitle}}</b></h4>
             </div>
+            <div class="col-lg-3 col-xl-2">
+                <div class="d-grid gap-2">
+                    <a href="{{ route('lifebuoys.index')}}" class="btn btn-primary text-white rounded-5"><i class="bi bi-arrow-left-circle me-2"></i>Back</a>
+                </div>
+            </div>
         </div>
         <div class="card rounded-4 pt-2 p-3 pb-0 border-0" style="margin-left:2%">
             <div class="row">
@@ -23,32 +28,29 @@
                         <div class="row">
                             <div class="col-md-12 mb-3">
                                 <label for="name" class="form-label">Name</label>
-                                <input class="form-control @error('name') is-invalid @enderror" type="text" name="name" id="name" value="{{ $errors->any() ? old('name') : $lifebuoy->name }}" placeholder="Enter Name">
+                                <input class="form-control @error('name') is-invalid @enderror rounded-5" type="text" name="name" id="name" value="{{ $errors->any() ? old('name') : $lifebuoy->name }}" placeholder="Enter Name">
                                 @error('name')
                                 <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div class="col-md-12 mb-3">
                                 <label for="description" class="form-label">Description</label>
-                                <input class="form-control @error('description') is-invalid @enderror" type="text" name="description" id="description" value="{{ $errors->any() ? old('description') : $lifebuoy->description }}" placeholder="Enter Description" required="">
+                                <input class="form-control @error('description') is-invalid @enderror rounded-5" type="text" name="description" id="description" value="{{ $errors->any() ? old('description') : $lifebuoy->description }}" placeholder="Enter Description" required="">
                                 @error('description')
                                 <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div class="col-md-12 mb-3">
                                 <label for="stock" class="form-label">Stock</label>
-                                <input class="form-control @error('stock') is-invalid @enderror" type="text" name="stock" id="stock" value="{{ $errors->any() ? old('stock') : $lifebuoy->stock }}" placeholder="Enter Stock" required="">
+                                <input class="form-control @error('stock') is-invalid @enderror rounded-5" type="text" name="stock" id="stock" value="{{ $errors->any() ? old('stock') : $lifebuoy->stock }}" placeholder="Enter Stock" required="">
                                 @error('stock')
                                 <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
                         <div class="row mt-5">
-                            <div class="col-md-6 d-grid">
-                                <a href="{{ route('lifebuoys.index') }}" class="btn btn-outline-dark btn-lg mt-3"><i class="bi-arrow-left-circle me-2"></i> Cancel</a>
-                            </div>
-                            <div class="col-md-6 d-grid">
-                                <button type="submit" class="btn btn-dark btn-lg mt-3"><i class="bi-check-circle me-2"></i> Save</button>
+                            <div class="col-md-12 d-grid">
+                                <button type="submit" class="btn btn-primary btn-lg mt-3 text-white rounded-5"><i class="bi-check-circle me-2"></i> Save</button>
                             </div>
                         </div>
                     </div>
