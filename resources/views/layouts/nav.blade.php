86c07614348c7adf903f52944a173cb97bb804e4
Iqbaal <ihakam93@gmail.com>
2023-07-20 15:16:53 +0700

    update nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -16,23 +16,23 @@
                         <img src="{{Vite::asset('resources/images/nisul-1.png')}}" alt="login form" class="img-fluid me-2" style="width:15%"> Tel-Park
                     </h4>
                 </div>
-                  <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu" style="padding:10%">
-                      <li class="nav-item">
+                  <ul class="nav nav-pills flex-column mb-sm-auto  align-items-center align-items-sm-start " id="menu" style="padding:10%;">
+                      <li class="nav-item mb-2">
                           <a href="{{ route('home') }}" class="nav-link align-middle px-0">
                               <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
                           </a>
                       </li>
-                      <li>
+                      <li class="mb-2">
                           <a href="{{ route('visitors.index') }}" class="nav-link px-0 align-middle">
                               <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Visitor</span></a>
                       </li>
-                      <li>
+                      <li class="mb-2">
                           <a href="{{ route('lifebuoys.index') }}" class="nav-link px-0 align-middle ">
                               <i class="fs-4 bi-life-preserver"></i> <span class="ms-1 d-none d-sm-inline">Lifebuoy</span></a>
                       </li>
-                      <li>
+                      <li class="mb-2">
                           <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                              <i class="fs-4 bi-tags-fill"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
+                              <i class="fs-4 bi-tags-fill"></i> <span class="ms-1 d-none d-sm-inline">Rent</span> </a>
                               <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
                           </ul>
                       </li>
@@ -45,12 +45,18 @@
           </div>
           <div class="col-lg-10">
               <div class="container mb-4" style="padding-left: 3%";>
-                  <div class="card rounded-4 pt-2 p-3 pb-0 border-0">
+                  <div class="card rounded-4 p-3 border-0">
                     <div class="row">
-                        <h6 style="color: #5493A2; font-weight:600">
-                            <img src="{{Vite::asset('resources/images/actor.png')}}" alt="login form" class="img-fluid me-2" style="width:6%"> Andre Ahmad
-                        </h6>
+                        <div class="hstack gap-3 text-primary">
+                            <img src="{{Vite::asset('resources/images/actor.png')}}" alt="login form" class="img-fluid" style="width:6%">
+                            <span style="font-weight: bold;"> Andre Ahmad </span>
+                            <div class="vr mx-3"></div>
+                            <div class="card bg-light border-0 p-2 rounded-5 text-primary">
+                                {{  now()->toDateString() }}
+                            </div>
+                          </div>
                     </div>
+
                       {{-- <i class="bi-person-circle me-1"></i> {{ Auth::user()->name }} --}}
                   </div>
               </div>
